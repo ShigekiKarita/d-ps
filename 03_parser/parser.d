0b0067332eb8bcab29bba62f23304fa384f01baf
karita <shigekikarita@gmail.com>
2019-08-11 21:21:41 +0900

    implement executable name parser

--- 03_parser/parser.d
+++ 03_parser/parser.d
@@ -20,7 +20,7 @@ union TokenValue
 {
     int number = 0;
     char onechar;
-    const(char)* name;
+    string name;
 }
 
 struct Token
@@ -56,8 +56,8 @@ int parseOne(Token* result)
 int parseOne(Token* result, int prev)
 {
     import core.stdc.stdio : fprintf, stderr;
-    import core.stdc.ctype : isdigit, isspace;
-    import cl_getc : cl_getc, pos;
+    import core.stdc.ctype : isdigit, isspace, isalpha, isgraph;
+    import cl_getc : cl_getc, pos, input;
 
     // parse nothing
     if (prev == EOF)
@@ -88,12 +88,30 @@ int parseOne(Token* result, int prev)
         }
         return prev;
     }
+    // parse executable name
+    else if (isalpha(prev))
+    {
+        import core.stdc.stdlib : malloc;
+        import core.stdc.string : strncpy;
+        result.lexType = LexicalType.executableName;
+        const s = input + pos - 1;
+        size_t n = 1;
+        while (isgraph(prev))
+        {
+            prev = cl_getc();
+            ++n;
+        }
+        auto p = cast(char*) malloc(char.sizeof * n);
+        strncpy(p, s, n);
+        result.value.name = cast(string) p[0 .. n - 1];
+        return prev;
+    }
 
-    fprintf(stderr, "unknown character: '%c' (at position %d)\n", prev, pos);
+    fprintf(stderr, "unknown character: '%c' (at position %d of the input '%s')\n", prev, pos, input);
     assert(false);
 }
 
-///
+/// test parse one number
 unittest
 {
     import cl_getc : cl_getc_set_src;
@@ -106,7 +124,7 @@ unittest
     assert(token.value.number == 123);
 }
 
-///
+/// test parse two numbers and one space
 unittest
 {
     import cl_getc : cl_getc_set_src;
@@ -126,4 +144,17 @@ unittest
     assert(prev == EOF);
     assert(token.lexType == LexicalType.number);
     assert(token.value.number == 345);
+}
+
+/// test parse an executable name
+unittest
+{
+    import cl_getc : cl_getc_set_src;
+
+    Token token;
+    cl_getc_set_src("add_");
+    auto prev = parseOne(&token);
+    assert(prev == EOF);
+    assert(token.lexType == LexicalType.executableName);
+    assert(token.value.name == "add_");
 }
