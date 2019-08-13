ccecd65ed6d635dc6f07fac64391d8f75cadf9af
karita <shigekikarita@gmail.com>
2019-08-14 02:32:53 +0900

    add ci

---
+++ .circleci/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
+
+source "$(curl -fsS  --retry 3 https://dlang.org/install.sh | bash -s $1 --activate)"
+dub test --build=unittest-cov
+cat dub.selections.json
+
+if [ "$DC" = dmd ]; then
+    bash <(curl -s https://codecov.io/bash) -s "*-grain-*.lst"
+fi
+
