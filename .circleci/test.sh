5d15e29d79c2ae2377a9c60aa043722cb83b0bbb
karita <shigekikarita@gmail.com>
2019-08-14 02:34:37 +0900

    update ci

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 source "$(curl -fsS  --retry 3 https://dlang.org/install.sh | bash -s $1 --activate)"
 dub test --build=unittest-cov
-cat dub.selections.json
+# cat dub.selections.json
 
 if [ "$DC" = dmd ]; then
     bash <(curl -s https://codecov.io/bash) -s "*-grain-*.lst"
